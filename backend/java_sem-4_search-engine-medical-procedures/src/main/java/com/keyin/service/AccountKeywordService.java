0f989782021820b235940fa2172fb2d5db76d132
kostiantyn <kostiantyn@test.com>
2024-04-02 20:23:22 -0230

    update: AccountKeywordService

---
+++ backend/java_sem-4_search-engine-medical-procedures/src/main/java/com/keyin/service/AccountKeywordService.java
@@ -0,0 +1,22 @@
+package com.keyin.service;
+
+import com.keyin.model.Account;
+import com.keyin.model.AccountKeyword;
+import com.keyin.model.Keyword;
+import com.keyin.repository.AccountKeywordRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class AccountKeywordService {
+    private final AccountKeywordRepository accountKeywordRepository;
+
+    public AccountKeywordService(AccountKeywordRepository accountKeywordRepository) {
+        this.accountKeywordRepository = accountKeywordRepository;
+    }
+
+    public AccountKeyword createAccountKeywordAssociation(Account account, Keyword keyword) {
+        return this.accountKeywordRepository.save(new AccountKeyword(account, keyword, LocalDateTime.now()));
+    }
+}
