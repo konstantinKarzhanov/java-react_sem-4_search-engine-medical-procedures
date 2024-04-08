07ad423c53db5cbf4de37dd3fbfb4b3b1678cd10
kostiantyn <kostiantyn@test.com>
2024-04-07 21:57:34 -0230

    removed: unused import

--- backend/java_sem-4_search-engine-medical-procedures/src/main/java/com/keyin/controller/AccountController.java
+++ backend/java_sem-4_search-engine-medical-procedures/src/main/java/com/keyin/controller/AccountController.java
@@ -7,7 +7,6 @@ import com.keyin.model.Account;
 import com.keyin.service.AccountService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
