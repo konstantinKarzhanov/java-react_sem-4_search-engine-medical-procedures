02a9a48117b1eb08fa78c0c4fec14bc178349f34
kostiantyn <kostiantyn@test.com>
2024-04-11 12:16:30 -0230

    removed: class based @CrossOrigin

--- backend/java_sem-4_search-engine-medical-procedures/src/main/java/com/keyin/controller/ProcedureController.java
+++ backend/java_sem-4_search-engine-medical-procedures/src/main/java/com/keyin/controller/ProcedureController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/procedure")
 public class ProcedureController {
