13b612ded51dc6f614f79dd38a01224ec895c629
kostiantyn <kostiantyn@test.com>
2024-04-11 12:13:02 -0230

    removed: class based @CrossOrigin
    modified: added cors configuration to securityFilterChain

--- backend/java_sem-4_search-engine-medical-procedures/src/main/java/com/keyin/config/SecurityConfig.java
+++ backend/java_sem-4_search-engine-medical-procedures/src/main/java/com/keyin/config/SecurityConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +41,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/", "/home").permitAll()
                         .requestMatchers("/account/registration").permitAll()
@@ -72,5 +78,21 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder);
 
         return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET","POST"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
--- backend/java_sem-4_search-engine-medical-procedures/src/main/java/com/keyin/controller/AccountController.java
+++ backend/java_sem-4_search-engine-medical-procedures/src/main/java/com/keyin/controller/AccountController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/account")
 public class AccountController {
