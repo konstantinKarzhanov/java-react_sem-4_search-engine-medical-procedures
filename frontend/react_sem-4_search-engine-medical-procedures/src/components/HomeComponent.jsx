18019d9f2f1e1f92a02a7c5bf6895133efd0d6b3
kostiantyn <kostiantyn@test.com>
2024-04-11 16:04:36 -0230

    modified: minor structure improvements

--- frontend/react_sem-4_search-engine-medical-procedures/src/components/HomeComponent.jsx
+++ frontend/react_sem-4_search-engine-medical-procedures/src/components/HomeComponent.jsx
@@ -1,6 +1,9 @@
 const HomeComponent = ({ data }) => {
     return (
-        <h1 className="fw--600">{JSON.stringify(data) || "no content yet"}</h1>
+        <h1 className="fw--600">
+            {JSON.stringify(data) ||
+                "no content to display yet, check the console"}
+        </h1>
     );
 };
 
