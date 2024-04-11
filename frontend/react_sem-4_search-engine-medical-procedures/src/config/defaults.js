b3988adc3675150bb98b12684cab5ba6a019dcda
kostiantyn <kostiantyn@test.com>
2024-04-11 20:45:20 -0230

    modified: added resourceMap with nav links

--- frontend/react_sem-4_search-engine-medical-procedures/src/config/defaults.js
+++ frontend/react_sem-4_search-engine-medical-procedures/src/config/defaults.js
@@ -1,3 +1,11 @@
 const baseURI = "http://localhost:8080";
+const resourceMap = new Map();
 
-export { baseURI };
+resourceMap.set("home", "/");
+resourceMap.set("procedure", "/procedure");
+resourceMap.set("account", "/account");
+resourceMap.set("registration", "/account/registration");
+resourceMap.set("login", "/account/login");
+resourceMap.set("logout", "/account/logout");
+
+export { baseURI, resourceMap };
