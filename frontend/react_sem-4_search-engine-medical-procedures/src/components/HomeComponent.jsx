c031d714318f51747fb55090648d3994890ec414
kostiantyn <kostiantyn@test.com>
2024-04-13 17:23:05 -0230

    home page: added description

--- frontend/react_sem-4_search-engine-medical-procedures/src/components/HomeComponent.jsx
+++ frontend/react_sem-4_search-engine-medical-procedures/src/components/HomeComponent.jsx
@@ -1,9 +1,31 @@
-const HomeComponent = ({ data }) => {
+import { Link } from "react-router-dom";
+
+import { registrationPath } from "../config/defaults.js";
+
+const HomeComponent = () => {
     return (
-        <h1 className="fw--600">
-            {JSON.stringify(data) ||
-                "no content to display yet, check the console"}
-        </h1>
+        <>
+            <h1 className="fw--600">Welcome to Procedure Archive</h1>
+            <div>
+                <p>
+                    Here, you can utilize our service to access comprehensive
+                    data pertaining to a wide range of medical procedures.
+                </p>
+                <p>
+                    To get started, registration is required for all users.
+                    Simply visit{" "}
+                    <Link to={registrationPath}>registration page</Link> to
+                    create your account. Once registered, you'll be seamlessly
+                    redirected to our login page.
+                </p>
+                <p>
+                    From there, you can effortlessly log in using your
+                    credentials. Once logged in, you can use keywords to search
+                    our database for information on the medical procedures
+                    you're interested in
+                </p>
+            </div>
+        </>
     );
 };
 
