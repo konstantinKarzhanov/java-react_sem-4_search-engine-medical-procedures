d647486f695ff4021505581e32fadba249c8c1a9
kostiantyn <kostiantyn@test.com>
2024-04-13 12:00:22 -0230

    modified: moved search logic one step deeper

--- frontend/react_sem-4_search-engine-medical-procedures/src/pages/ProcedurePage.jsx
+++ frontend/react_sem-4_search-engine-medical-procedures/src/pages/ProcedurePage.jsx
@@ -1,22 +1,12 @@
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
 import ProcedureComponent from "../components/ProcedureComponent.jsx";
 
-import { getData } from "../api/serverAPI.js";
-import { baseURI } from "../config/defaults.js";
-
 const ProcedurePage = () => {
-    const { pathname, search } = useLocation();
-    const [data, setData] = useState();
-
-    const url = baseURI + pathname + search;
-
-    useEffect(() => {
-        getData(url);
-    }, []);
-
-    return <ProcedureComponent data={data} />;
+    return (
+        <>
+            <h1 className="fw--600">Procedures Archive</h1>
+            <ProcedureComponent />
+        </>
+    );
 };
 
 export default ProcedurePage;
