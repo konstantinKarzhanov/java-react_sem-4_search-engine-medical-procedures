13ed7944f1588583fc3667c8ca021d79515ded4c
kostiantyn <kostiantyn@test.com>
2024-04-13 17:21:40 -0230

    modified: added userData state
    contains data about authenticated user or 'Anonymous' if user is not authenticated

--- frontend/react_sem-4_search-engine-medical-procedures/src/context/MainContext.jsx
+++ frontend/react_sem-4_search-engine-medical-procedures/src/context/MainContext.jsx
@@ -4,13 +4,23 @@ const MainContext = createContext();
 
 const MainContextProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [userData, setUserData] = useState("Anonymous");
 
     useEffect(() => {
         console.log("MainContextProvider, isAuthenticated: " + isAuthenticated);
+        console.log("MainContextProvider, userData: " + userData);
+        isAuthenticated || setUserData("Anonymous");
     }, [isAuthenticated]);
 
     return (
-        <MainContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+        <MainContext.Provider
+            value={{
+                isAuthenticated,
+                setIsAuthenticated,
+                userData,
+                setUserData,
+            }}
+        >
             {children}
         </MainContext.Provider>
     );
